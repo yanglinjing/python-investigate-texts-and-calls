e26f2224587efe3ca839ad24f681ddcebb22ca66
yanglinjing@gmail.com <yanglinjing@gmail.com>
2018-03-30 18:21:26 +1100

    Add another method of Task1

---
+++ Task1_method2.py
@@ -0,0 +1,26 @@
+"""
+下面的文件将会从csv文件中读取读取短信与电话记录，
+你将在以后的课程中了解更多有关读取文件的知识。
+"""
+import csv
+with open('texts.csv', 'r') as f:
+    reader = csv.reader(f)
+    texts = list(reader)
+
+with open('calls.csv', 'r') as f:
+    reader = csv.reader(f)
+    calls = list(reader)
+
+
+"""
+任务1：
+短信和通话记录中一共有多少电话号码？每个号码只统计一次。
+输出信息：
+"There are <count> different telephone numbers in the records."
+"""
+
+phone = sum([[text[0], text[1]] for text in texts],[]) + sum([[call[0], call[1]] for call in calls],[])
+#phone = sum([[1,2],[2,3]],[])
+#print(phone)
+phone_count = len(set(phone))
+print("There are {} different telephone numbers in the records.".format(phone_count))
